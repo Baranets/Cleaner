7228e180fb48e3d25ae8d0229591ac971f64c876
Baranets <mxa788@gmail.com>
2018-08-12 03:06:59 +0300

    Second Commit Testing: -commiting from IDEA

--- src/main/java/Antivirus/Antivirus.java
+++ src/main/java/Antivirus/Antivirus.java
@@ -1,4 +1,19 @@
 package Antivirus;
 
-public class Antivirus {
+import AutoControl.RobotDefault;
+
+import java.awt.*;
+
+public class Antivirus extends RobotDefault {
+
+    public Antivirus() throws AWTException {
+    super();
+    }
+
+    public Antivirus(Robot robot, int defDelay) throws AWTException {
+        super(robot, defDelay);
+    }
+
+
+
 }
