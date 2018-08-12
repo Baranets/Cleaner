ff6275899f575480a0ceffffc33a34c0afa37c3f
Baranets <mxa788@gmail.com>
2018-08-12 05:03:02 +0300

    Third Added: -updating keys for antivirus -message after finishing of the thread

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class Main extends Application {
 
@@ -27,8 +18,6 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/Interface/MainWindow.fxml"));
         primaryStage.setTitle("Main");
         primaryStage.setScene(new Scene(root));
-//        primaryStage.initStyle(StageStyle.UNDECORATED);
-
         primaryStage.setOnCloseRequest(e -> System.exit(0));
         primaryStage.show();
     }
